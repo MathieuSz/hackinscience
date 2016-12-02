7bb522efd22b989dbf0e0cd319917424c609007c
MathieuSz <mathieu.szotowski@cri-paris.org>
2016-12-02 20:43:55 +0100

    Exercise 8

--- exercises/008/solution.py
+++ exercises/008/solution.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2.7
 def main():
-	letters = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "!"]
-	for i in letters:
-		print(i)
+letters = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "!"]
+for i in letters:
+print(i)

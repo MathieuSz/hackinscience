473cc3b68cf06ad6cd321e1d2fe36bea92a920d1
MathieuSz <mathieu.szotowski@cri-paris.org>
2016-12-06 18:40:44 +0100

    Exercise 8

--- exercises/008/solution.py
+++ exercises/008/solution.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python2.7
-def main():
-letters = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "!"]
-for i in letters:
-print(i)
+string = "Hello world !"
+for x in string:
+	print x
